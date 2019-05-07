d0cc58982269f2912e48613938df5d1ac8550bf3
Nick Lehnert <nick.lehnert1@gmail.com>
2019-05-07 12:40:31 -0500

    Added function to reveal tiles that surround a revealed 0, added more comments

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -12,41 +12,43 @@ namespace Minesweeper
 {
     public partial class Game : Form
     {
-        private int numRows;
-        private int numCols;
+        private static int numRows;
+        private static int numCols;
         private int numBombs;
         private Random random;
-        private Tile[][] tiles;
+        private static Tile[][] tiles;
         private bool gameWon;
         private bool gameLost;
 
         public Game(int rows, int cols, int numBombs)
         {
             InitializeComponent();
-            this.numRows = rows;
-            this.numCols = cols;
+            numRows = rows;
+            numCols = cols;
             this.numBombs = numBombs;
             tiles = new Tile[rows][];
+
+            //Initialize Tile array
             for(int i = 0; i < rows; i++)
             {
                 tiles[i] = new Tile[cols];
             }
             random = new Random();
 
-
+            //Create and place tiles
             for (int i = 0; i < numRows; i++)
             {
                 for (int j = 0; j < numCols; j++)
                 {
-                    //TODO: Create and place tiles, populate with bombs, then update tile values
-                    Tile t = new Tile();
-                    t.Location = new Point(i * 25, j * 25);
+                    Tile t = new Tile(i, j);
+                    t.Location = new Point(i * 35, j * 35);
                     this.Controls.Add(t);
                     tiles[i][j] = t;
                 }
             }
             placeBombs();
-
+            this.BackColor = Color.FromArgb(200, 200, 200);
+            this.Size = new Size(40 * numRows, 40 * numCols);
         }
 
         private void Game_Load(object sender, EventArgs e)
@@ -63,11 +65,14 @@ namespace Minesweeper
                 //Keep placing bombs at random locations until all have been placed
                 int x = random.Next(0, numRows);
                 int y = random.Next(0, numCols);
+
                 //If random tile is not already a bomb, make it a bomb and increment bombsPlaced
                 if(!tiles[x][y].IsBomb())
                 {
                     tiles[x][y].PlantBomb();
                     bombsPlaced++;
+
+                    //Update surrounding tile values to reflect addition of a new bomb
                     updateAdjacentTiles(x, y);
                 }
 
@@ -80,9 +85,23 @@ namespace Minesweeper
             {
                 for(int j = y-1; j <= y+1; j++)
                 {
-                    if((i >= 0 && j >= 0 && i < this.numRows && j < this.numCols) && tiles[i][j].GetValue() != -1)
+                    if((i >= 0 && j >= 0 && i < numRows && j < numCols) && tiles[i][j].GetValue() != -1)
                     {
                         tiles[i][j].IncrementValue();
+                    }
+                }
+            }
+        }
+
+        public static void RevealSurroundingTiles(int x, int y)
+        {
+            for(int i = x-1; i <= x+1; i++)
+            {
+                for(int j = y-1; j <= y+1; j++)
+                {
+                    if ((i >= 0 && j >= 0 && i < numRows && j < numCols) && !tiles[i][j].Revealed)
+                    {
+                        tiles[i][j].Reveal();
                     }
                 }
             }
